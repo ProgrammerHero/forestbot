45d989a10dc33e5d01162b78a668c450e5ab6896
Owen Davison <odavison@cs.dal.ca>
2016-02-03 10:52:22 -0400

    Update trigger implementation to use events.

--- forestbot.lua
+++ forestbot.lua
@@ -8,21 +8,18 @@
 -- Central bot namespace
 --------------------------------------------------------------------------------
 bot = {}
+bot.handlers = {}
 bot.functions = {}
 bot.debug = true
-
-if bot.debug then
-  bot.debugMessage = print
-else
-  bot.debugMessage = function(s) end
-end
 
 --------------------------------------------------------------------------------
 -- Initializer function.  Will be executed when this script file is loaded.
 --------------------------------------------------------------------------------
 function bot.init()
   if bot.debug then
-    bot.debugMessage = print
+    bot.debugMessage = function(s)
+      print(">> " .. s)
+    end
   else
     bot.debugMessage = function(s) end
   end
@@ -41,6 +38,7 @@ function bot.init()
   package.loaded["behaviourtree.behaviourtree"] = nil
   behaviourtree = require("behaviourtree.behaviourtree")
 
+  bot.initHandlers()
   bot.reset()
 end
 
@@ -82,7 +80,7 @@ function bot.reset()
   bot.items.hasFood = true 
   bot.items.hasWater = true
 
-  bot.inventory = {}
+  bot.items.inventory = {}
 
   -- should probably init inventory here
   -- and stats
@@ -123,8 +121,78 @@ end
 -- Enable inventory parsing triggers and request inventory from the mud.
 --------------------------------------------------------------------------------
 function bot.functions.updateInventory()
-  enableTrigger("request_inventory")
+  enableTrigger("refresh inventory")
   send("inventory")
+end
+
+--------------------------------------------------------------------------------
+-- Function to register an event handler with Mudlet's event system
+-- Stores registered handlers in the bot.handlers namespace.
+-- We guarantee that only one event will be fired per line from the mud.
+--------------------------------------------------------------------------------
+function bot.addHandler(eventName, handlerName, handlerFunc)
+  bot.debugMessage("Adding bot.handlers." .. handlerName ..
+  " to handle \"" .. eventName .. "\" event.")
+  bot.handlers[handlerName] = handlerFunc
+  registerAnonymousEventHandler(eventName, "bot.handlers." .. handlerName)
+end
+
+--------------------------------------------------------------------------------
+-- Function to remove an event handler
+-- As of Feb 2016, Mudlet's event system cannot unregister an event handler,
+-- so we instead change the lua function registered as a handler to a no-op.
+--------------------------------------------------------------------------------
+function bot.removeHandler(eventName, handlerName)
+  bot.handlers[handlerName] = function() end
+end
+
+--------------------------------------------------------------------------------
+-- Handlers for triggered events
+--------------------------------------------------------------------------------
+function bot.initHandlers()
+  bot.addHandler("hungerEvent", "hunger",
+  function(eventName, hungerLevel)
+    bot.debugMessage("Setting bot.needs.hunger to " .. hungerLevel)
+    bot.needs.hunger = hungerLevel
+  end
+  )
+
+  bot.addHandler("thirstEvent", "thirst",
+  function(eventName, thirstLevel)
+    bot.debugMessage("Setting bot.needs.thirst to " .. thirstLevel)
+    bot.needs.thirst = thirstLevel
+  end
+  )
+
+  bot.addHandler("inventoryUpdated", "inventory",
+  function()
+    bot.debugMessage("Implement inventory update handler.")
+  end
+  )
+
+  bot.addHandler("noFood", "noFood",
+  function()
+    bot.debugMessage("Setting bot.items.hasFood = false")
+    bot.items.hasFood = false
+  end
+  )
+
+  bot.addHandler("noWater", "noWater",
+  function()
+    bot.items.hasWater = false
+  end
+  )
+
+  bot.addHandler("scoreUpdated", "score",
+  function()
+    bot.debugMessage("Implement score update handler.")
+  end
+  )
+
+  bot.addHandler("prompt", "prompt",
+  function()
+  end
+  )
 end
 
 --------------------------------------------------------------------------------
